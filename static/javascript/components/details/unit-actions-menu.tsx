a048513d32098adcd5c220bb9127ab743c757bfa
Artyom Trityak <art.trityak@gmail.com>
2018-11-22 17:15:52 -0500

    unit menu icons

--- static/javascript/components/details/unit-actions-menu.tsx
+++ static/javascript/components/details/unit-actions-menu.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { connect } from "react-redux";
 import { Icon } from "../utils";
-import shieldIcon from "../../../assets/svg/shield.svg";
+import attackIcon from "../../../assets/svg/battles.svg";
 import moneyIcon from "../../../assets/svg/money_bag.svg";
-import behindIcon from "../../../assets/svg/behind.svg";
-import avoidIcon from "../../../assets/svg/avoid.svg";
-import holdIcon from "../../../assets/svg/tower.svg";
-import noAidIcon from "../../../assets/svg/stop.svg";
-import consumeIcon from "../../../assets/svg/consume.svg";
-import bridgeIcon from "../../../assets/svg/bridge.svg";
+import castIcon from "../../../assets/svg/magic_wand.svg";
+import formIcon from "../../../assets/svg/troll.svg";
+import entertainIcon from "../../../assets/svg/lute.svg";
+import studyIcon from "../../../assets/svg/study.svg";
+import teachIcon from "../../../assets/svg/behind.svg";
+import advanceIcon from "../../../assets/svg/sword.svg";
+import produceIcon from "../../../assets/svg/wrench_hammer.svg";
 
 const UnitActionsMenu = props => {
   return (
@@ -21,25 +22,31 @@ const UnitActionsMenu = props => {
           <div className="col-6">
             <span className="dropdown-item-text">Fast Actions</span>
             <div className="dropdown-divider" />
-            <button className="dropdown-item" type="button" title="[attack title]">
+            <button className="dropdown-item actions-menu__menu-item" type="button" title="[attack title]">
+              <Icon {...attackIcon} className="actions-menu__icon" />
               ATTACK
             </button>
-            <button className="dropdown-item" type="button">
+            <button className="dropdown-item actions-menu__menu-item" type="button">
+              <Icon {...moneyIcon} className="actions-menu__icon" />
               BUY
             </button>
-            <button className="dropdown-item" type="button">
+            <button className="dropdown-item actions-menu__menu-item" type="button">
+              <Icon {...castIcon} className="actions-menu__icon" />
               CAST
             </button>
-            <button className="dropdown-item" type="button">
+            <button className="dropdown-item actions-menu__menu-item" type="button">
+              <Icon {...castIcon} className="actions-menu__icon" />
               COMBAT
             </button>
-            <button className="dropdown-item" type="button">
+            <button className="dropdown-item actions-menu__menu-item" type="button">
+              <Icon {...formIcon} className="actions-menu__icon" />
               FORM
             </button>
-            <button className="dropdown-item" type="button">
+            <button className="dropdown-item actions-menu__menu-item" type="button">
               GIVE
             </button>
-            <button className="dropdown-item" type="button">
+            <button className="dropdown-item actions-menu__menu-item" type="button">
+              <Icon {...formIcon} className="actions-menu__icon" />
               NAME
             </button>
             <button className="dropdown-item dropdown-toggle" type="button">
@@ -49,25 +56,34 @@ const UnitActionsMenu = props => {
           <div className="col-6">
             <span className="dropdown-item-text">Month Actions</span>
             <div className="dropdown-divider" />
-            <button className="dropdown-item" type="button">
+            <button className="dropdown-item actions-menu__menu-item" type="button">
+              <Icon {...advanceIcon} className="actions-menu__icon" />
               ADVANCE
             </button>
-            <button className="dropdown-item" type="button">
+            <button className="dropdown-item actions-menu__menu-item" type="button">
+              <Icon {...entertainIcon} className="actions-menu__icon" />
               ENTERTAIN
             </button>
-            <button className="dropdown-item" type="button">
+            <button className="dropdown-item actions-menu__menu-item" type="button">
               MOVE
             </button>
-            <button className="dropdown-item" type="button">
+            <button className="dropdown-item actions-menu__menu-item" type="button">
+              <Icon {...produceIcon} className="actions-menu__icon" />
+              PRODUCE
+            </button>
+            <button className="dropdown-item actions-menu__menu-item" type="button">
+              <Icon {...studyIcon} className="actions-menu__icon" />
               STUDY
             </button>
-            <button className="dropdown-item" type="button">
+            <button className="dropdown-item actions-menu__menu-item" type="button">
+              <Icon {...moneyIcon} className="actions-menu__icon" />
               TAX
             </button>
-            <button className="dropdown-item" type="button">
+            <button className="dropdown-item actions-menu__menu-item" type="button">
+              <Icon {...teachIcon} className="actions-menu__icon" />
               TEACH
             </button>
-            <button className="dropdown-item" type="button">
+            <button className="dropdown-item actions-menu__menu-item" type="button">
               WORK
             </button>
           </div>
