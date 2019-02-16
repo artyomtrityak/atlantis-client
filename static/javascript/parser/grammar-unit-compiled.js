c6a60abe7620206cf6086d10082f564c1a2e2ce2
Artyom Trityak <art.trityak@gmail.com>
2019-02-15 19:57:03 -0800

    unit parser

--- static/javascript/parser/grammar-unit-compiled.js
+++ static/javascript/parser/grammar-unit-compiled.js
@@ -49,13 +49,29 @@
   };
 
   const unitParser = d => {
-    console.log("U1", d);
+    // console.log("U1", d);
     return d;
   };
 
   const objectParser = d => {
-    console.log("O1", d);
+    // console.log("O1", d);
     return d;
+  };
+
+  const unitSkills = d => {
+    return array2String(d);
+  };
+
+  const unitFaction = d => {
+    return array2String(d);
+  };
+
+  const unitName = d => {
+    return array2String(d);
+  };
+
+  const unitFlags = d => {
+    return array2String(d);
   };
   var grammar = {
     Lexer: undefined,
@@ -277,7 +293,6 @@
         postprocess: d => ({ title: array2String(d), z: parseInt(String(4) + String(d[1]), 10) })
       },
       { name: "UNIT_PARSER", symbols: ["UNIT_PARSER_ITEMS"], postprocess: unitParser },
-      { name: "UNIT_PARSER", symbols: ["NPC_PARSER_ITEMS"], postprocess: unitParser },
       { name: "UNIT_PARSER", symbols: [/[+]/, "_", "TEXT", { literal: "." }], postprocess: objectParser },
       { name: "UNIT_PARSER_ITEMS$ebnf$1", symbols: ["UNIT_SECTION"] },
       {
@@ -289,37 +304,25 @@
       },
       {
         name: "UNIT_PARSER_ITEMS",
-        symbols: [/[*-]/, "_", "UNIT_NAME", "UNIT_FACTION_NAME", "UNIT_PARSER_ITEMS$ebnf$1"],
+        symbols: [/[*-]/, "_", "UNIT_NAME", "UNIT_PARSER_ITEMS$ebnf$1"],
         postprocess: d => {
-          console.log("SECTION:", d);
+          // console.log("SECTION:", d);
           return d;
         }
       },
-      { name: "NPC_PARSER_ITEMS$ebnf$1", symbols: ["UNIT_SECTION"] },
+      { name: "UNIT_SECTION", symbols: ["UNIT_SECTION_ITEM", /[.,]/], postprocess: array2String },
+      { name: "UNIT_SECTION_ITEM", symbols: ["_", "UNIT_SKILLS"], postprocess: unitSkills },
+      { name: "UNIT_SECTION_ITEM", symbols: ["_", "UNIT_FACTION_NAME"] },
+      { name: "UNIT_SECTION_ITEM", symbols: ["_", "UNIT_FLAGS"], postprocess: unitFlags },
+      { name: "UNIT_SECTION_ITEM$ebnf$1", symbols: [/[^,.]/] },
       {
-        name: "NPC_PARSER_ITEMS$ebnf$1",
-        symbols: ["NPC_PARSER_ITEMS$ebnf$1", "UNIT_SECTION"],
+        name: "UNIT_SECTION_ITEM$ebnf$1",
+        symbols: ["UNIT_SECTION_ITEM$ebnf$1", /[^,.]/],
         postprocess: function arrpush(d) {
           return d[0].concat([d[1]]);
         }
       },
-      {
-        name: "NPC_PARSER_ITEMS",
-        symbols: [/[-]/, "_", "UNIT_NAME", "NPC_PARSER_ITEMS$ebnf$1"],
-        postprocess: d => {
-          console.log("SECTION:", d);
-          return d;
-        }
-      },
-      { name: "UNIT_SECTION$ebnf$1", symbols: [/[^,.]/] },
-      {
-        name: "UNIT_SECTION$ebnf$1",
-        symbols: ["UNIT_SECTION$ebnf$1", /[^,.]/],
-        postprocess: function arrpush(d) {
-          return d[0].concat([d[1]]);
-        }
-      },
-      { name: "UNIT_SECTION", symbols: ["UNIT_SECTION$ebnf$1", /[.,]/], postprocess: array2String },
+      { name: "UNIT_SECTION_ITEM", symbols: ["UNIT_SECTION_ITEM$ebnf$1"], postprocess: array2String },
       { name: "UNIT_NAME$ebnf$1", symbols: [/[^,.()]/] },
       {
         name: "UNIT_NAME$ebnf$1",
@@ -330,8 +333,8 @@
       },
       {
         name: "UNIT_NAME",
-        symbols: ["UNIT_NAME$ebnf$1", "_", { literal: "(" }, "INT", { literal: ")" }, /[.,]/],
-        postprocess: array2String
+        symbols: ["UNIT_NAME$ebnf$1", "_", { literal: "(" }, "INT", { literal: ")" }, { literal: "," }],
+        postprocess: unitName
       },
       { name: "UNIT_FACTION_NAME$ebnf$1", symbols: [/[^,.()]/] },
       {
@@ -343,9 +346,157 @@
       },
       {
         name: "UNIT_FACTION_NAME",
-        symbols: ["UNIT_FACTION_NAME$ebnf$1", "_", { literal: "(" }, "INT", { literal: ")" }, /[.,]/],
-        postprocess: array2String
-      }
+        symbols: ["UNIT_FACTION_NAME$ebnf$1", "_", { literal: "(" }, "INT", { literal: ")" }, { literal: "," }],
+        postprocess: unitFaction
+      },
+      {
+        name: "UNIT_FLAGS$string$1",
+        symbols: [
+          { literal: "a" },
+          { literal: "v" },
+          { literal: "o" },
+          { literal: "i" },
+          { literal: "d" },
+          { literal: "i" },
+          { literal: "n" },
+          { literal: "g" }
+        ],
+        postprocess: function joiner(d) {
+          return d.join("");
+        }
+      },
+      { name: "UNIT_FLAGS", symbols: ["UNIT_FLAGS$string$1"], postprocess: () => ({ type: "FLAG", name: "avoiding" }) },
+      {
+        name: "UNIT_FLAGS$string$2",
+        symbols: [{ literal: "b" }, { literal: "e" }, { literal: "h" }, { literal: "i" }, { literal: "n" }, { literal: "d" }],
+        postprocess: function joiner(d) {
+          return d.join("");
+        }
+      },
+      { name: "UNIT_FLAGS", symbols: ["UNIT_FLAGS$string$2"], postprocess: () => ({ type: "FLAG", name: "behind" }) },
+      {
+        name: "UNIT_FLAGS$string$3",
+        symbols: [
+          { literal: "h" },
+          { literal: "o" },
+          { literal: "l" },
+          { literal: "d" },
+          { literal: "i" },
+          { literal: "n" },
+          { literal: "g" }
+        ],
+        postprocess: function joiner(d) {
+          return d.join("");
+        }
+      },
+      { name: "UNIT_FLAGS", symbols: ["UNIT_FLAGS$string$3"], postprocess: () => ({ type: "FLAG", name: "holding" }) },
+      {
+        name: "UNIT_FLAGS$string$4",
+        symbols: [
+          { literal: "c" },
+          { literal: "o" },
+          { literal: "n" },
+          { literal: "s" },
+          { literal: "u" },
+          { literal: "m" },
+          { literal: "i" },
+          { literal: "n" },
+          { literal: "g" },
+          { literal: " " },
+          { literal: "f" },
+          { literal: "a" },
+          { literal: "c" },
+          { literal: "t" },
+          { literal: "i" },
+          { literal: "o" },
+          { literal: "n" },
+          { literal: "'" },
+          { literal: "s" },
+          { literal: " " },
+          { literal: "f" },
+          { literal: "o" },
+          { literal: "o" },
+          { literal: "d" }
+        ],
+        postprocess: function joiner(d) {
+          return d.join("");
+        }
+      },
+      {
+        name: "UNIT_FLAGS",
+        symbols: ["UNIT_FLAGS$string$4"],
+        postprocess: () => ({ type: "FLAG", name: "consuming", subtype: "faction" })
+      },
+      {
+        name: "UNIT_FLAGS$string$5",
+        symbols: [
+          { literal: "s" },
+          { literal: "h" },
+          { literal: "a" },
+          { literal: "r" },
+          { literal: "i" },
+          { literal: "n" },
+          { literal: "g" }
+        ],
+        postprocess: function joiner(d) {
+          return d.join("");
+        }
+      },
+      { name: "UNIT_FLAGS", symbols: ["UNIT_FLAGS$string$5"], postprocess: () => ({ type: "FLAG", name: "sharing" }) },
+      {
+        name: "UNIT_FLAGS$string$6",
+        symbols: [
+          { literal: "w" },
+          { literal: "a" },
+          { literal: "l" },
+          { literal: "k" },
+          { literal: "i" },
+          { literal: "n" },
+          { literal: "g" },
+          { literal: " " },
+          { literal: "b" },
+          { literal: "a" },
+          { literal: "t" },
+          { literal: "t" },
+          { literal: "l" },
+          { literal: "e" },
+          { literal: " " },
+          { literal: "s" },
+          { literal: "p" },
+          { literal: "o" },
+          { literal: "i" },
+          { literal: "l" },
+          { literal: "s" }
+        ],
+        postprocess: function joiner(d) {
+          return d.join("");
+        }
+      },
+      { name: "UNIT_FLAGS", symbols: ["UNIT_FLAGS$string$6"], postprocess: () => ({ type: "FLAG", name: "spoils", subtype: "walking" }) },
+      {
+        name: "UNIT_SKILLS$string$1",
+        symbols: [
+          { literal: "S" },
+          { literal: "k" },
+          { literal: "i" },
+          { literal: "l" },
+          { literal: "l" },
+          { literal: "s" },
+          { literal: ":" }
+        ],
+        postprocess: function joiner(d) {
+          return d.join("");
+        }
+      },
+      { name: "UNIT_SKILLS$ebnf$1", symbols: [/[^,.]/] },
+      {
+        name: "UNIT_SKILLS$ebnf$1",
+        symbols: ["UNIT_SKILLS$ebnf$1", /[^,.]/],
+        postprocess: function arrpush(d) {
+          return d[0].concat([d[1]]);
+        }
+      },
+      { name: "UNIT_SKILLS", symbols: ["UNIT_SKILLS$string$1", "_", "UNIT_SKILLS$ebnf$1", { literal: "." }], postprocess: array2String }
     ],
     ParserStart: "UNIT_PARSER"
   };
--- static/javascript/parser/grammar-unit/index.ne
+++ static/javascript/parser/grammar-unit/index.ne
@@ -2,13 +2,29 @@
 
 @{%
   const unitParser = (d) => {
-    console.log("U1", d);
+    // console.log("U1", d);
     return d;
   }
 
   const objectParser = (d) => {
-    console.log("O1", d);
+    // console.log("O1", d);
     return d;
+  }
+
+  const unitSkills = (d) => {
+    return array2String(d);
+  }
+
+  const unitFaction = (d) => {
+    return array2String(d);
+  }
+
+  const unitName = (d) => {
+    return array2String(d);
+  }
+
+  const unitFlags = (d) => {
+    return array2String(d);
   }
 %}
 
@@ -18,15 +34,36 @@ UNIT_PARSER ->
 
 UNIT_PARSER_ITEMS ->
   [*-] _ UNIT_NAME UNIT_SECTION:+ {% (d) => {
-    console.log("SECTION:", d);
+    // console.log("SECTION:", d);
     return d;
   } %}
 
 UNIT_SECTION ->
-  [^,.]:+ [.,] {% array2String %}
+  UNIT_SECTION_ITEM [.,] {% array2String %}
+
+UNIT_SECTION_ITEM ->
+  _ UNIT_SKILLS {% unitSkills %}
+  | _ UNIT_FACTION_NAME
+  | _ UNIT_FLAGS {% unitFlags %}
+  | [^,.]:+ {% array2String %}
+
 
 UNIT_NAME ->
-  [^,.()]:+ _ "(" INT ")" [.,] {% array2String %}
+  [^,.()]:+ _ "(" INT ")" "," {% unitName %}
+
 
 UNIT_FACTION_NAME ->
-  [^,.()]:+ _ "(" INT ")" [.,] {% array2String %}
+  [^,.()]:+ _ "(" INT ")" "," {% unitFaction %}
+
+
+UNIT_FLAGS ->
+  "avoiding" {% () => ({ type: "FLAG", name: "avoiding" }) %}
+  | "behind" {% () => ({ type: "FLAG", name: "behind" }) %}
+  | "holding" {% () => ({ type: "FLAG", name: "holding" }) %}
+  | "consuming faction's food" {% () => ({ type: "FLAG", name: "consuming", subtype: "faction" }) %}
+  | "sharing" {% () => ({ type: "FLAG", name: "sharing" }) %}
+  | "walking battle spoils" {% () => ({ type: "FLAG", name: "spoils", subtype: "walking" }) %}
+
+
+UNIT_SKILLS ->
+  "Skills:" _ [^,.]:+ "." {% array2String %}
